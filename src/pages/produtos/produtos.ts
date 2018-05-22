539536953ce80acedfa6d1b5365944f044f124d9
Mateus Renato <mateusspadacio@gmail.com>
2018-05-21 23:44:28 -0300

    Adicionando refresher

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -13,9 +13,10 @@ import { LoadingControllerHelper } from '../../controllers/loading.controller';
 export class ProdutosPage {
 
   items: ProdutoDTO[] = [];
-  categoria: CategoriaDTO = {id: '', nome: '', urlImage: ''};
+  categoria: CategoriaDTO = { id: '', nome: '', urlImage: '' };
+  refresher: any;
 
-  constructor(public navCtrl: NavController, 
+  constructor(public navCtrl: NavController,
     public navParams: NavParams,
     public produtoService: ProdutoService,
     public loading: LoadingControllerHelper) {
@@ -34,26 +35,39 @@ export class ProdutosPage {
     }
 
     this.produtoService.findByCategoria(this.categoria.id)
-    .subscribe((items: any) => {
-      this.items = items.content;
-      this.produtoService.findImages(this.items)
-      .then((items) => {
-        this.items = items;
-        this.loading.hideLoadingWithTime(1000);
+      .subscribe((items: any) => {
+        this.items = items.content;
+        this.produtoService.findImages(this.items)
+          .then((items) => {
+            this.items = items;
+            this.loading.hideLoadingWithTime(1000);
+            this.completeRefresher();
+          })
+          .catch((error) => {
+            this.loading.hideLoading();
+            this.completeRefresher();
+          })
       })
-      .catch((error) => {
-        this.loading.hideLoading();
-      })
-    })
   }
 
   openProdutoDetail(item: ProdutoDTO): void {
-    this.navCtrl.push('ProdutoDetailPage', {produto: item});
+    this.navCtrl.push('ProdutoDetailPage', { produto: item });
   }
 
   private redirectIfUndefined(): void {
     this.loading.hideLoading();
     this.navCtrl.setRoot('CategoriasPage');
+  }
+
+  doRefresh(refresher) {
+    this.refresher = refresher;
+    this.loadingProdutos();
+  }
+
+  private completeRefresher(): void {
+    if (this.refresher != undefined) {
+      this.refresher.complete();
+    } 
   }
 
 }
